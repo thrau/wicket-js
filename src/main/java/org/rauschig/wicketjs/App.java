e9ef33ba293d25e5154db1a9f2a43e76f53ee600
Thomas Rausch <thomas@rauschig.org>
2014-02-05 16:21:53 +0100

    Create project

---
+++ src/main/java/org/rauschig/wicketjs/App.java
@@ -0,0 +1,17 @@
+package org.rauschig.wicketjs;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * Returns true!
+     */
+    public boolean isTrue() {
+        return true;
+    }
+}
